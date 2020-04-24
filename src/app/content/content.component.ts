fc79f6620f6cd3d43c6f49a0be6a94a6a79fccc4
Ratnesh Kumar Singh <ratnesh@metadesignsolutions.co.uk>
2020-04-24 21:16:28 +0530

    commit

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
-
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { MyValidation } from '../validators/custom.validators';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -7,31 +8,51 @@ import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angu
 })
 export class ContentComponent implements OnInit {
 
-  @ViewChild('f') form;
+
   @Input() products;
   @Output() buyClicked = new EventEmitter();
 
   title = 'Ratnesh';
 
+  form: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [MyValidation.cannotContainSpace]),
+      password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      address: new FormGroup({
+        city: new FormControl(),
+        country: new FormControl()
+      }),
+      companies: new FormArray([])
+    });
   }
 
-  addToCart(id) {
-    this.buyClicked.emit(id);
+  removeEmailValidator() {
+    this.form.get('email').setValidators([]);
+    this.form.get('email').updateValueAndValidity();
   }
 
-  seeInConsole() {
-    console.log(this.title);
+  addCompany() {
+    let company = new FormGroup({
+      title: new FormControl(),
+      designation: new FormControl()
+    });
+    (this.form.get('companies') as FormArray).push(company);
   }
 
-  show() {
-    console.log(this.form);
+  removeCompany(i) {
+    (this.form.get('companies') as FormArray).removeAt(i);
   }
 
-  submit(){
-    console.log(this.form);
+  get companies() {
+    return (this.form.get('companies') as FormArray).controls;
+  }
+
+  submit() {
+    console.log(this.form.value);
   }
 
 }
---
+++ src/app/validators/custom.validators.ts
@@ -0,0 +1,14 @@
+
+
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+export class MyValidation {
+    static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
+        if ((control.value as string).indexOf(' ') != -1) {
+            return {
+                spaceError: true
+            }
+        }
+        return null;
+    }
+}
